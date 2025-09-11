54d85ef657fa0a39bc666f7fd15f9022d269bb3f
Badmus2005 <tchidehoudodjivirgile@gmail.com>
2025-09-11 10:53:06 +0100

    Changement

--- dbconfig.js
+++ dbconfig.js
@@ -3,10 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'db_votes',
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
