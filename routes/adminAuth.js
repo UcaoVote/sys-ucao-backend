fc78100c2b81d2bd08cce58eebe19c0a559dd197
Badmus2005 <tchidehoudodjivirgile@gmail.com>
2025-10-08 21:27:08 +0100

    Changement

--- routes/adminAuth.js
+++ routes/adminAuth.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import { authenticateToken, requireAdmin } from '../middlewares/auth.js';
 import adminService from '../controllers/adminManager.js';
 const router = express.Router();
 
@@ -120,48 +119,6 @@ router.post('/register', async (req, res) => {
 }
 );
 
-
-router.get('/me', authenticateToken, requireAdmin, async (req, res) => {
-    try {
-        const admin = await adminService.getAdminProfile(req.user.id);
-
-        if (!admin) {
-            return res.status(404).json({ message: "Admin introuvable" });
-        }
-
-        res.json({
-            nom: admin.nom,
-            prenom: admin.prenom,
-            poste: admin.poste,
-            email: admin.email,
-            role: admin.role
-        });
-    } catch (err) {
-        console.error("Erreur admin/me:", err);
-        res.status(500).json({ message: "Erreur serveur" });
-    }
-});
-
-
-router.put('/update', authenticateToken, requireAdmin, async (req, res) => {
-    try {
-        const { nom, prenom, email } = req.body;
-        const updatedAdmin = await adminService.updateAdminProfile(req.user.id, nom, prenom, email);
-
-        res.json({
-            message: "Profil mis à jour avec succès",
-            admin: {
-                nom: updatedAdmin.nom,
-                prenom: updatedAdmin.prenom,
-                email: updatedAdmin.email
-            }
-        });
-    } catch (err) {
-        console.error("Erreur admin/update:", err);
-        res.status(500).json({ message: "Erreur lors de la mise à jour" });
-    }
-}
-);
 
 
 
